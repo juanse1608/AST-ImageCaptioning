6defd23dd3d0b13e5f313f1e4f6dbca9d56eb677
Juan Sebastián Corredor Rodriguez <js.uniandes10@gmail.com>
2021-05-02 21:26:40 -0500

    Add code for webapp using streamlit

---
+++ Scripts/app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from predictions_type_0 import show_predict_page, evaluate
+
+import SessionState
+
+
+
+# Run the predict page
+show_predict_page()
+
+# File uploader allows user to add their own image
+uploaded_file = st.file_uploader(label="Upload Image", type=["png", "jpeg", "jpg"])
+# Setup session state to remember state of app so refresh isn't always needed
+# See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
+session_state = SessionState.get(pred_button=False)
+
+# Create logic for app flow
+if not uploaded_file:
+    st.warning("Please upload an image.")
+    st.stop()
+else:
+    session_state.uploaded_image = uploaded_file.read()
+    st.image(session_state.uploaded_image, use_column_width=True)
+    pred_button = st.button("Predict")
+
+# Did the user press the predict button?
+if pred_button:
+    session_state.pred_button = True 
+
+# And if they did...
+if session_state.pred_button:
+    session_state.pred_caption, session_state.attention_plot = evaluate(session_state.uploaded_image)
+    st.write(f"Prediction: {session_state.pred_caption}")
+
+    # Create feedback mechanism (building a data flywheel)
+    session_state.feedback = st.selectbox(
+        "Is this correct?",
+        ("Select an option", "Yes", "No"))
+    if session_state.feedback == "Select an option":
+        pass
+    elif session_state.feedback == "Yes":
+        st.write("Thank you for your feedback!")
+        # Log prediction information to terminal (this could be stored in Big Query or something...)
+        # print(update_logger(image=session_state.image,
+        #                    model_used=MODEL,
+        #                    pred_class=session_state.pred_class,
+        #                    pred_conf=session_state.pred_conf,
+        #                    correct=True))
+    elif session_state.feedback == "No":
+        session_state.correct_class = st.text_input("What should the correct caption be?")
+        if session_state.correct_class:
+            st.write("Thank you for that, we'll use your help to make our model better!")
+            # Log prediction information to terminal (this could be stored in Big Query or something...)
+            #print(update_logger(image=session_state.image,
+            #                    model_used=MODEL,
+            #                    pred_class=session_state.pred_class,
+            #                    pred_conf=session_state.pred_conf,
+            #                    correct=False,
+            #                    user_label=session_state.correct_class))
---
+++ Scripts/predictions_type_0.py
@@ -0,0 +1,187 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import json
+import pickle
+import os
+
+### Atributes 
+# Parameters
+with open('Data/Objects/parameters.json') as json_file:
+    parameters = json.load(json_file)
+
+max_length = parameters['MAX_LENGTH']
+attention_features_shape = parameters['ATTENTION_FEATURES_SHAPE']
+vocab_size = parameters['VOCAB_SIZE']
+units = parameters['UNITS']
+embedding_dim = parameters['EMBEDDING_DIMENSION']
+
+### Classes
+
+# The CNN encoder used
+class CNN_Encoder(tf.keras.Model):
+    # Since you have already extracted the features and dumped it
+    # This encoder passes those features through a Fully connected layer
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        # shape after fc == (batch_size, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+        
+class RNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+        self.attention = BahdanauAttention(self.units)
+
+    def call(self, x, features, hidden):
+        # defining attention as a separate model
+        context_vector, attention_weights = self.attention(features, hidden)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+
+        # shape == (batch_size, max_length, hidden_size)
+        x = self.fc1(output)
+
+        # x shape == (batch_size * max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
+
+        # output shape == (batch_size * max_length, vocab)
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.units))
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, features, hidden):
+        # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
+
+        # hidden shape == (batch_size, hidden_size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+        # attention_hidden_layer shape == (batch_size, 64, units)
+        attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                             self.W2(hidden_with_time_axis)))
+
+        # score shape == (batch_size, 64, 1)
+        # This gives you an unnormalized score for each image feature.
+        score = self.V(attention_hidden_layer)
+
+        # attention_weights shape == (batch_size, 64, 1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+### Objects
+
+# Encoder, decoder, optimizer
+encoder = CNN_Encoder(embedding_dim)
+decoder = RNN_Decoder(embedding_dim, units, vocab_size)
+optimizer = tf.keras.optimizers.Adam()
+
+# Pretrained Inception V3
+image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-1].output
+
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+# Tokenize
+with open('Data/Objects/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+# Up to date weights
+checkpoint_path = "Data/TrainCheckpoints/TrainType0/"
+ckpt = tf.train.Checkpoint(encoder=encoder, decoder=decoder, optimizer=optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+start_epoch = 0
+if ckpt_manager.latest_checkpoint:
+    start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
+    # Restoring the latest checkpoint in checkpoint_path
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+
+### Functions
+
+# Load an image and adjust it to InceptionV3 networks
+def image_to_v3_format(image_path):
+    #img = tf.io.decode_image(image_path, channels=3)
+    img = tf.image.decode_image(image_path, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
+
+### Predictions
+def evaluate(image):
+    attention_plot = np.zeros((max_length, attention_features_shape))
+
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dims(image_to_v3_format(image)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0],
+                                                 -1,
+                                                 img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input,
+                                                         features,
+                                                         hidden)
+
+        attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return  ' '.join(result), attention_plot
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot
+
+
+def show_predict_page():
+    st.title('Machine Learning Web App - Image Captioning')
+    st.write('''### Give Some Info''')
+

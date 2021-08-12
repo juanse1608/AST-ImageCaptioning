4f780497795918e2967fbbd09edecdc36377b323
Juan Sebastián Corredor Rodriguez <js.uniandes10@gmail.com>
2021-08-11 20:16:30 -0500

    Cloud storage uploading

--- Scripts/app.py
+++ Scripts/app.py
@@ -6,11 +6,20 @@ import session_state
 import json
 import pandas as pd
 import PIL.Image as Image
+import session_info
 import io
-import os 
+import os
+from google.cloud import storage, bigquery 
 
 # Feedback type
-cloud_feedback = False
+cloud_feedback = True
+try:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'GOOGLE_ACC_CREDENTIALS.json'
+except:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = st.secrets["GOOGLE_ACC_CREDENTIALS"]
+storage_cl = storage.Client()
+bigq_cl = bigquery.Client()
+bucket_name = 'img-captioning-feedback'
 
 # Run the predict page
 st.title('Machine Learning Web App - Image Captioning')
@@ -50,7 +59,53 @@ if value == 'Argmax':
     # Create feedback mechanism (building a data flywheel)
     session_state.feedback = st.selectbox("Is this correct?", ("Select and option", "Yes", "No"))
     if cloud_feedback:
-        pass
+        if session_state.feedback == "Select an option":
+            pass
+        elif session_state.feedback == "Yes":
+            feedback_bucket = storage_cl.get_bucket(bucket_name)
+            blobs = storage_cl.list_blobs(bucket_name, prefix='images/', delimiter='/')
+            try: 
+                files = []
+                for f in blobs:
+                    f_n = f.name.split('/')[1]
+                    try:
+                        files.append(int(f_n.split('.')[0]))
+                    except:
+                        pass
+                new_file = str(max(files)+1) + '.jpg'
+            except:
+                new_file = '0.jpg'
+            img = Image.open(io.BytesIO(session_state.uploaded_image))
+            img.save(f'{new_file}')
+            blob = feedback_bucket.blob(f'images/{new_file}')
+            blob.upload_from_filename(new_file)
+            os.remove(f'{new_file}')
+            new_caption = pd.DataFrame({'PATH': [new_file], 'CAPTION': val})
+            st.write("Thank you for your feedback!")
+        elif session_state.feedback == "No":
+            session_state.correct_class = st.text_input("What should the correct caption be?")
+            if session_state.correct_class:
+                try: 
+                    files = os.listdir(os.getcwd() + '/Data/Feedback/Images/')
+                    files = [f for f in files if not f.startswith('.')]
+                    files = [int(f.split('.')[0]) for f in files]
+                    new_file = str(max(files)+1) + '.jpg'
+                except:
+                    new_file = '0.jpg'
+                img = Image.open(io.BytesIO(session_state.uploaded_image))
+                img.save(f'Data/Feedback/Images/{new_file}')
+                new_caption = pd.DataFrame({'PATH': [new_file], 'CAPTION': session_state.correct_class})
+                try:
+                    caps = pd.read_csv('Data/Feedback/Captions/captions.csv')
+                    caps = pd.concat([caps, new_caption], axis=0).reset_index(drop=True)
+                    caps.to_csv('Data/Feedback/Captions/captions.csv', index=False)
+                except:
+                    new_caption.to_csv('Data/Feedback/Captions/captions.csv', index=False)
+                st.write("Thank you for that, we'll use your help to make our model better!")
+                # Log prediction information to terminal (this could be stored in Big Query or something like that)
+        
+        
+
     else:
         if session_state.feedback == "Select an option":
             pass
